d744d920327e132fe4996dd9ea73d177d22ebb06
Pablo Dorrío Vázquez <pablo.dorrio.vazquez@gmail.com>
2023-08-06 12:35:44 +0200

    feat: integrate react router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
