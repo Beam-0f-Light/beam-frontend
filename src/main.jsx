17c520545f85a3778f5ae60bd92bf3a7e0969e90
Pablo Dorrío Vázquez <pablo.dorrio.vazquez@gmail.com>
2023-08-06 21:49:39 +0200

    fix: react router base path

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/flowfuel-frontend",
     element: <App />,
   },
 ]);
